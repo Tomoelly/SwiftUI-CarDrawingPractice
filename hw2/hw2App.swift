fe3748769eaded4c0fd0087cf772d6011f2d3f0f
Tomoelly <ellyhuang26@gmail.com>
2025-03-24 19:36:06 +0800

    Initial Commit

---
+++ hw2/hw2App.swift
@@ -0,0 +1,17 @@
+//
+//  hw2App.swift
+//  hw2
+//
+//  Created by 黃友慧 on 2025/3/24.
+//
+
+import SwiftUI
+
+@main
+struct hw2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
